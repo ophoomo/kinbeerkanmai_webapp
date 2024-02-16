4a4035c67080050a9e86ec9f60855bbc99349eac
THANAPHOOM AUNCHIT <me@thanaphoom.dev>
2024-02-17 01:28:50 +0700

    🎉 (Begin): New my project
    new my project

---
+++ src/components/KinBeerComponent.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+  import Button from 'primevue/button';
+import { ref } from 'vue';
+  const count = ref(0);
+  const listMessage = ref([
+    {
+      title: "ไปกินเบียร์กันไหม",
+      yes: "ไปปปกันนนน",
+      no: "ดูก่อนน"
+    },
+    {
+      title: "เดี๋ยวเลี้ยง",
+      yes: "ไปดิ",
+      no: "ไม่เอาๆ กินแล้วได้ไรวะ"
+    },
+    {
+      title: "ก็ได้เสมอออ ไงไอสัสส",
+      yes: "อ้าวววไปดิ",
+      no: "ไม่ๆ"
+    },
+    {
+      title: "ไปเถอะ น้าาาา",
+      yes: "ไปก็ได้ๆ",
+      no: "ไม่ไป"
+    }
+  ]);
+
+  const changeState = () => {
+    if(count.value < listMessage.value.length - 1) count.value++;
+  }
+
+  const scaleButton = () => {
+    return 1 + (count.value/5)
+  }
+
+  const GoKinBeer = () => {
+    alert("ไปดิ จะรออยู่ไม");
+  }
+
+</script>
+
+<template>
+  <div class="flex gap-3 flex-col">
+    <h1 class="text-6xl mb-10 font-bold">{{ listMessage[count].title }}</h1>
+    <Button @click="GoKinBeer" :style="{transform: `scale(${scaleButton()},${scaleButton()})`}">{{ listMessage[count].yes }}</Button>
+    <Button @click="changeState" severity="danger">{{ listMessage[count].no }}</Button>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import KinBeerComponent from '@/components/KinBeerComponent.vue'
+</script>
+
+<template>
+  <main>
+    <div class="flex w-full h-screen bg-red-200 justify-center items-center">
+      <KinBeerComponent />
+    </div>
+  </main>
+</template>
